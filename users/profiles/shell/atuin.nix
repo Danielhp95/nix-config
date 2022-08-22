9998a18ef7c6df59f5209b31734fb732d562bd21
Dr.Caca <caca@gmail.com>
2022-08-22 11:31:52 +0200

    Added atuin

--- users/profiles/shell/atuin.nix
+++ users/profiles/shell/atuin.nix
@@ -1,5 +1,7 @@
 {
 
+  # Remember that there are some manual steps
+  # required after installation
   programs.atuin.enable = true;
   programs.atuin.enableZshIntegration = true;
   programs.atuin.settings.auto_sync = false; # disable sync
--- users/sarios/default.nix
+++ users/sarios/default.nix
@@ -8,6 +8,7 @@
     imports = with profiles; [
       git
       direnv
+      shell.atuin
       shell.zsh
       shell.starship
       shell.vim.neovim
@@ -23,20 +24,20 @@
       mako
     ];
     home.packages = with pkgs; [
-      discord           # Chat server
-      firefox           # Web browser
-      tmux              # Terminal mutiplexer
-      tdesktop          # Telegram desktop
-      exa               # Better ls
-      bat               # Better cat
-      delta             # Git diff pager
-      element-desktop   # Matrix server client
-      mpv               # Media player
-      spotify           # Music music music
-      feh               # image viewer
-      deluge            # Torrent client
-      bottom            # like htop but cooler
-      libnotify         # To have notify-send command
+      discord # Chat server
+      firefox # Web browser
+      tmux # Terminal mutiplexer
+      tdesktop # Telegram desktop
+      exa # Better ls
+      bat # Better cat
+      delta # Git diff pager
+      element-desktop # Matrix server client
+      mpv # Media player
+      spotify # Music music music
+      feh # image viewer
+      deluge # Torrent client
+      bottom # like htop but cooler
+      libnotify # To have notify-send command
     ];
 
   };
@@ -47,8 +48,8 @@
     isNormalUser = true;
     hashedPassword = "$6$FSsxxOy7h3MFBXy4$wJ8fHywHWLDX30M7MFReqUUKOkDVuDWfAUbzMG6hf/z4siwpLavvzWFTly3Dp0duvlbEAJSXd2vPF.Ni7HRyp.";
     extraGroups = [
-      "wheel"     # For sudo users
-      "plugdev"   # Razer keyboard
+      "wheel" # For sudo users
+      "plugdev" # Razer keyboard
       "openrazer" # require by the openrazer daemon
     ];
     shell = pkgs.zsh;
